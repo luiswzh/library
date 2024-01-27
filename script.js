8db629f93f4f5dbb46159598ccde668af62e1bf3
Luis Wen <69808402+luiswzh@users.noreply.github.com>
2024-01-27 18:06:30 -0500

    Add method to sort books in library

--- script.js
+++ script.js
@@ -25,11 +25,34 @@ library.removeBook = function(book) {
     }
 };
 
+//Method to sort books in library
+library.sortBooks = function(order=this.sortOrder, parameter=this.sortType){
+    if(order === 'az'){
+        this.books.sort((a,b)=>{
+            if(a[parameter]<b[parameter]){
+                return -1;
+            } else if (a[parameter]>b[parameter]){
+                return 1;
+            } else
+            return 0;
+        })
+    } else {
+        this.books.sort((a,b)=>{
+            if(a[parameter]<b[parameter]){
+                return 1;
+            } else if (a[parameter]>b[parameter]){
+                return -1;
+            } else
+            return 0;
+        })
+    }
+};
+
 //Creates book constructor
 function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.pages = Number(pages);
     this.status = status;
 };
 
