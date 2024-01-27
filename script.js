045b36e3d17ab08c512bf9ccd333b19f040f148e
Luis Wen <69808402+luiswzh@users.noreply.github.com>
2024-01-27 17:21:06 -0500

    Creates library object and setup event listeners to change sort settings

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-dialog = document.querySelector('dialog');
+//Element Selectors
+const dialog = document.querySelector('dialog');
+const azSort = document.querySelector('.sort-az');
+const zaSort = document.querySelector('.sort-za');
+const sortCriteria = document.querySelector('#sort-criteria');
+const libraryTable = document.querySelector('table');
+const addBookButton = document.querySelector('.add-new');
+
+//Creates library object
+let library = {
+    books: [],
+    sortOrder: 'az',
+    sortType: 'title',
+};
+
+//Sort order selection
+//AZ sort:
+azSort.addEventListener('click',()=>{
+    azSort.classList.add('active');
+    zaSort.classList.remove('active');
+    library.sortOrder = 'az';
+});
+//ZA sort:
+zaSort.addEventListener('click',()=>{
+    zaSort.classList.add('active');
+    azSort.classList.remove('active');
+    library.sortOrder = 'za';
+});
+
+//Sort type:
+sortCriteria.addEventListener('change',()=>{
+    library.sortType = sortCriteria.value;
+});
